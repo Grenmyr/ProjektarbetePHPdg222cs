e596dec4dc42959d2bcfc7d01444c322555a9718
Grenmyr <dg222cs@student.lnu.se>
2014-10-06 17:30:29 +0200

    #Content Har arbetat i classModel. Bara löst problem, nästa commit kommer vara samma funktionalitet men refaktorerat.

--- src/model/InterpretModel.php
+++ src/model/InterpretModel.php
@@ -1,9 +1,10 @@
 <?php
 namespace model;
 
+require_once(__DIR__ . "/ClassModel.php");
+
 
 class InterpretModel {
-    const VALIDCHARSINCLASS = '/[^a-z0-9|()-;\-]+/i';
     const MATCHBETWEENSQUAREBRACKETS = '/\[(\w+)((?:\|\+?\w+)*)((?:\|\+?\w+\(\))*)\]/'; //Full objects
 
 
@@ -15,31 +16,26 @@ class InterpretModel {
         $this->inputString = $string;
 
         $classArray = $this->find(self::MATCHBETWEENSQUAREBRACKETS,$string);
-        //$sanitizedClassArray= $this->sanitize(self::VALIDCHARSINCLASS,$classArray);
 
-        //$attributelist = $this->findAttributes(self::MATCHBETWEENLINE,$sanitizedClassArray);
 
-        var_dump($classArray);
+
+        foreach ($classArray as $class){
+            $this->classArray[] = new ClassModel($class);
+        }
+
+        //var_dump($this->classArray);
         //var_dump($sanitizedClassArray);
 
         return ($classArray);
 
     }
-    private function findAttributes($regex,$classArray){
-        foreach ($classArray as $value){
-            return preg_replace($regex, $value, $value);
-        }
-    }
+
     private function find($regex,$string){
         preg_match_all($regex,$string,$classArray, PREG_SET_ORDER);
 
         return $classArray;
     }
-    private function sanitize($regex,$array){
-        foreach ($array as $value){
-            return preg_replace($regex, '', $value);
-        }
-    }
+
 }
 /**
  * Created by PhpStorm.
