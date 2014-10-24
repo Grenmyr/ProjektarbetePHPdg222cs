f273cd56adf5eb3761b58f8971db3fb624c688e1
Grenmyr <dg222cs@student.lnu.se>
2014-10-24 23:03:07 +0200

    lag till exceptions i repository

--- settings.php
+++ settings.php
@@ -9,22 +9,13 @@ class Settings {
         * model/repository/Repository
 
     */
-    /*
+
     //Test Settings
     public static $DB_USERNAME = "root";
     public static $DB_PASSWORD = '';
     public static $DB_HOST = '127.0.0.1';
     public static $DB_NAME = 'logindb';
     public static $ROOT_PATH = 'ProjektPHP/ProjektarbetePHPdg222cs';
-    */
-    //Published Settings
-
-    public static $DB_USERNAME = 'dgrenmyr_nu';
-    public static $DB_PASSWORD = 'EPDL8Vdx';
-    public static $DB_HOST = 'dgrenmyr.nu.mysql';
-    public static $DB_NAME = 'dgrenmyr_nu';
-    //public static $DB_CONNECTION = 'mysql:host=dgrenmyr.nu.mysql;dbname=dgrenmyr_nu';
-    public static $ROOT_PATH = 'ProjektarbetePHPdg222cs';
 
 }
 
